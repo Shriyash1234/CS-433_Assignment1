49e0f5d37dcc7074a586fcfb7c0dc8ca8ed0b2db
Shriyash <mandavekarshriyash@gmail.com>
2023-09-11 01:03:57 +0530

    change 1st code

--- assignment1_1.c
+++ assignment1_1.c
@@ -1,71 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <pcap.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    // Create a raw socket to capture TCP packets.
-    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(1);
+// Structure to represent a unique TCP flow tuple
+struct TCPFlow {
+    char client_ip[INET_ADDRSTRLEN];
+    char server_ip[INET_ADDRSTRLEN];
+    uint16_t client_port;
+    uint16_t server_port;
+};
+
+// Compare function for TCP flow tuples
+int compare_flow(const void *a, const void *b) {
+    const struct TCPFlow *flow_a = (const struct TCPFlow *)a;
+    const struct TCPFlow *flow_b = (const struct TCPFlow *)b;
+
+    int cmp = strcmp(flow_a->client_ip, flow_b->client_ip);
+    if (cmp == 0) {
+        cmp = flow_a->client_port - flow_b->client_port;
+        if (cmp == 0) {
+            cmp = strcmp(flow_a->server_ip, flow_b->server_ip);
+            if (cmp == 0) {
+                cmp = flow_a->server_port - flow_b->server_port;
+            }
+        }
+    }
+    return cmp;
+}
+
+int main(int argc, char *argv[]) {
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
+
+    // Open the network interface "eth0" for packet capture
+    handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Error opening interface: %s\n", errbuf);
+        return 1;
     }
 
-    struct sockaddr_in sa;
-    memset(&sa, 0, sizeof(struct sockaddr_in));
+    // Array to store unique TCP flow tuples
+    struct TCPFlow flow_list[1024];  // Assuming a maximum of 1024 unique flows
+    int flow_count = 0;
 
-    // Set up socket address structure.
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(0);  // Listen on any port.
-    sa.sin_addr.s_addr = htonl(INADDR_ANY);  // Listen on any network interface.
+    while (true) {
+        struct pcap_pkthdr header;
+        const u_char *packet = pcap_next(handle, &header);
 
-    // Bind the socket to the network interface.
-    if (bind(sockfd, (struct sockaddr*)&sa, sizeof(struct sockaddr_in)) < 0) {
-        perror("Binding failed");
-        close(sockfd);
-        exit(1);
-    }
-
-    while (1) {
-        char buffer[65536];  // Buffer to store received packets.
-
-        // Receive a packet into the buffer.
-        ssize_t data_size = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
-
-        if (data_size < 0) {
-            perror("Packet receive error");
-            close(sockfd);
-            exit(1);
+        if (packet == NULL) {
+            continue;  // If no packet is available, continue the loop
         }
 
-        // Extract the IP header and TCP header from the received packet.
-        struct ip* ip_header = (struct ip*)buffer;
-        struct tcphdr* tcp_header = (struct tcphdr*)(buffer + ip_header->ip_hl * 4);
+        // Extract IP and TCP headers from the packet
+        struct ip *ip_header = (struct ip *)(packet + 14);
+        struct tcphdr *tcp_header = (struct tcphdr *)(packet + 14 + ip_header->ip_hl * 4);
 
-        char source_ip[INET_ADDRSTRLEN];
-        char dest_ip[INET_ADDRSTRLEN];
+        // Extract the flow tuple from the packet
+        struct TCPFlow current_flow;
+        inet_ntop(AF_INET, &(ip_header->ip_src), current_flow.client_ip, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &(ip_header->ip_dst), current_flow.server_ip, INET_ADDRSTRLEN);
+        current_flow.client_port = ntohs(tcp_header->th_sport);
+        current_flow.server_port = ntohs(tcp_header->th_dport);
 
-        // Convert source and destination IP addresses to human-readable form.
-        inet_ntop(AF_INET, &(ip_header->ip_src), source_ip, INET_ADDRSTRLEN);
-        inet_ntop(AF_INET, &(ip_header->ip_dst), dest_ip, INET_ADDRSTRLEN);
+        // Check if the flow is already in the list of unique flows
+        bool flow_exists = false;
+        for (int i = 0; i < flow_count; i++) {
+            if (compare_flow(&flow_list[i], &current_flow) == 0) {
+                flow_exists = true;
+                break;  // Flow already exists, so no need to add it again
+            }
+        }
 
-        // Get source and destination port numbers.
-        int source_port = ntohs(tcp_header->th_sport);
-        int dest_port = ntohs(tcp_header->th_dport);
+        if (!flow_exists) {
+            // Add the new flow to the list of unique flows
+            flow_list[flow_count] = current_flow;
+            flow_count++;
 
-        // Print the captured packet information.
-        printf("Source IP: %s, Source Port: %d, Dest IP: %s, Dest Port: %d\n",
-               source_ip, source_port, dest_ip, dest_port);
+            // Print the flow information and the current flow count
+            printf("Flow Count: %d, Client IP: %s, Client Port: %d, Server IP: %s, Server Port: %d\n",
+                   flow_count, current_flow.client_ip, current_flow.client_port,
+                   current_flow.server_ip, current_flow.server_port);
+        }
     }
 
-    // Close the socket when done.
-    close(sockfd);
-
+    // Close the pcap handle when the program exits
+    pcap_close(handle);
     return 0;
 }
